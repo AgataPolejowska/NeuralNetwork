f1f1983dd444f43a4b09a9c808979e80ffc012a2
AgataPolejowska <44722312+AgataPolejowska@users.noreply.github.com>
2020-02-23 09:28:15 +0100

    Add files via upload

---
+++ CNN.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+
+def generate_model():
+
+    model = tf.keras.Sequential([
+
+        tf.keras.layers.Conv2D(32, filter_size=3, activation='relu'),
+        tf.keras.layers.MaxPool2D(pool_size=2, strides=2),
+
+        tf.keras.layers.Conv2D(64, filter_size=3, activation='relu'),
+        tf.keras.layers.MaxPool2D(pool_size=2, strides=2),
+
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(1024, activation='relu'),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+
+    return model
